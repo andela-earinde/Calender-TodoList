20f1f767a51445905fb029dcb18201501f3b1ecc
UD <udswagz@gmail.com>
2015-03-16 18:40:13 +0100

    updated ui design

--- app/listCalendar/controllers.js
+++ app/listCalendar/controllers.js
@@ -1,5 +1,12 @@
 app.controller('HomeController', ['$scope', function($scope){
 
+    $scope.showNewListForm = false ;
+    $scope.listCount = 20 ;
+
+    $scope.toggleNewListForm = function(){
+        $scope.showNewListForm = !$scope.showNewListForm;
+    };
+
     $scope.lists = [
         { name: 'Market' },
         { name: 'School' },
