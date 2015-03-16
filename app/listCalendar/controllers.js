48ead3be2a82d434cef5747fc7b0aaee2a08cbc7
UD <udswagz@gmail.com>
2015-03-16 13:25:04 +0100

    added material design stuff

--- app/listCalendar/controllers.js
+++ app/listCalendar/controllers.js
@@ -1,4 +1,10 @@
 app.controller('HomeController', ['$scope', function($scope){
 
-    
+    $scope.lists = [
+        { name: 'Market' },
+        { name: 'School' },
+        { name: 'Office' },
+        { name: 'shop' },
+    ];
+
 }]);
