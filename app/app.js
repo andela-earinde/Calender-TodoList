b534f764b389b04e85e63e61ecbde0d60fbbc2a9
UD <udswagz@gmail.com>
2015-03-16 11:30:30 +0100

    created basic setups

--- app/app.js
+++ app/app.js
@@ -1,1 +1,23 @@
-var app = angular.module('taskApp', []);
+var app = angular.module('taskApp', [
+    'ngRoute',
+    'ngMaterial',
+    'ngAnimate',
+    'ngAria',
+    'taskApp.controllers'
+]);
+
+app.config(['$routeProvider',
+    function($routeProvider){
+        $routeProvider.
+            when('/', {
+                templateUrl: 'app/views/home.html',
+                controller: 'HomeController'
+            }).
+            when('/404', {
+                templateUrl: 'app/views/404.html'
+            }).
+            otherwise({
+                redirectTo: '/404'
+            });
+    }
+]);
---
+++ app/controllers.js
@@ -0,0 +1,5 @@
+var controllers = angular.module('taskApp.controllers', [ ]);
+
+controllers.controller('HomeController', ['$scope', function($scope){
+
+}]);
