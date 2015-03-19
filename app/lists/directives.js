ab6950a5ce192a8f372f289d9e6d1ccea29d5f76
Udo <udo@Monsieur-Slim.local>
2015-03-19 14:47:52 +0100

    fixed directive issue

--- app/lists/directives.js
+++ app/lists/directives.js
@@ -1,5 +1,5 @@
 
-app.directive('lists', ['listService','$localStorage', function(listService,$localStorage){
+app.directive('lists', ['listService', function(listService){
 
     return{
         restrict: 'E',
@@ -8,40 +8,40 @@ app.directive('lists', ['listService','$localStorage', function(listService,$loc
         templateUrl: 'app/lists/templates/list-tpl.html',
         link: function(scope, element, attributes){
             scope.listName = attributes.listName;
+            var oldname;
             scope.listIndex = attributes.listIndex ;
+
+            var el = element.find('.listItem');
+            var edit = el.find('.fa-edit');
+            var del = el.find('.fa-times-circle-o');
+            var save = el.find('.fa-check');
+            var nameDiv  = el.find('.list-name');
 
             // Manipulate DOM here
             element.on('click', function(){
                 angular.element('.listItem').removeClass('active');
-                var el = element.find('.listItem');
                 el.addClass('active');
-                var edit = el.find('.fa-edit');
-                var del = el.find('.fa-times-circle-o');
-                var save = el.find('.fa-check');
-                var nameDiv  = el.find('.list-name');
-                //var toolsDiv = el.find('.list-tools');
+                oldname = attributes.listName;
+            });
 
+            //Edit
+            edit.on('click', function(e){
+                e.preventDefault();
+                save.show(); edit.hide(); del.hide();
+                nameDiv.addClass('editOn').attr('contentEditable',true);
+            });
 
-                //Edit function
-                edit.on('click', function(e){
-                    e.preventDefault();
-                    save.show(); edit.hide(); del.hide();
-                    nameDiv.addClass('editOn').attr('contentEditable',true);
-                });
-
-                save.on('click', function(e){
-                    e.preventDefault();
-                    var content = nameDiv.html();
-                    if(/\w+\s\w+/.test(content)){
-                        alert('list name has to be one word, no spaces');
-                    }else{
-                        nameDiv.removeClass('editOn').removeAttr('contentEditable');
-                        save.hide(); edit.show(); del.show();
-                        listService.edit(attributes.listName,content);
-                    }
-                });
-
-
+            // Save
+            save.on('click', function(e){
+                e.preventDefault();
+                var content = nameDiv.html();
+                if(/\w+\s\w+/.test(content)){
+                    alert('list name has to be one word, no spaces');
+                }else{
+                    nameDiv.removeClass('editOn').removeAttr('contentEditable');
+                    save.hide(); edit.show(); del.show();
+                    listService.edit(oldname,content);
+                }
             });
         }
     };
